3f25525149e47d1cc90719e70b3aba96c2f049df
Nicolas Lopez <nicolaslopezbravo@gmail.com>
2018-01-06 15:23:34 -0500

    added new fxml file to work on

--- Application/src/gui/GreetingController.java
+++ Application/src/gui/GreetingController.java
@@ -3,8 +3,13 @@ package gui;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class GreetingController
 {
@@ -14,8 +19,29 @@ public class GreetingController
     public Button print;
     public ImageView logo;
 
-    public void uploadFile(ActionEvent actionEvent) {
+    public void uploadFile(ActionEvent actionEvent)
+    {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File(s)");
+        File file = fileChooser.showOpenDialog(new Stage());
+
+        if(file != null)
+        {
+            openFile(file);
+        }
+
         fileUpload.setText("File Uploaded!");
+    }
+
+    private void openFile(File file)
+    {
+        Desktop desktop = Desktop.getDesktop();
+
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            fileUpload.setText("File Not Found");
+        }
     }
 
     public void print(ActionEvent actionEvent) {
