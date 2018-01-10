135cb448be5c8f6b52fb1e05f6daab4aab644a17
Nicolas Lopez <nicolaslopezbravo@gmail.com>
2018-01-09 22:40:27 -0500

    added JPanel to tracer

--- Application/src/gui/Interfaces/MainMenu/MenuController.java
+++ Application/src/gui/Interfaces/MainMenu/MenuController.java
@@ -1,12 +1,21 @@
 package gui.Interfaces.MainMenu;
 
 import gui.Interfaces.Greeting.GreetingController;
+import javafx.embed.swing.SwingNode;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
 
+import javax.swing.*;
 import java.io.File;
+import java.io.IOException;
 
-public class MenuController {
-    
-    
+public class MenuController{
+
+    public TabPane TPane;
     private File model = null;
     
     private ModelController modelController;
@@ -14,11 +23,36 @@ public class MenuController {
     private TraceController traceController;
     private RotationController rotationController;
     
-    
-    public void init()
+    public void initialize()
     {
+        // We assume it is the router system
+
+        boolean isHotWire = false;
+        String fxmlToLoad = "Trace.fxml";
+        if(isHotWire)
+        {
+            fxmlToLoad = "Rotation.fxml";
+        }
+
+       // We add a tab dynamically depending on which fxml file we will load
+        try {
+
+            Tab tabThird = (FXMLLoader.load(this.getClass().getResource(fxmlToLoad)));
+            TPane.getTabs().add(tabThird);
+
+            Pane pane = (Pane)tabThird.getContent();
+            SwingNode swingNode = (SwingNode) pane.getChildren().get(0);
+            JPanel panel = new JPanel();
+            swingNode.setContent(panel);
+
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        /*
         model = new File(GreetingController.getFileNames().get(0));
-        
+
         modelController = new ModelController();
         gcodeController = new GcodeController();
         traceController = new TraceController();
@@ -28,6 +62,7 @@ public class MenuController {
         gcodeController.init(model);
         traceController.init();
         rotationController.init();
+        */
     }
     
     
