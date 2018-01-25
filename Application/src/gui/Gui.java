208eddebff6717de685cd685a57b98256b5597eb
Nicolas Lopez <nicolaslopezbravo@gmail.com>
2018-01-25 16:32:27 -0500

    back to normal

--- Application/src/gui/Gui.java
+++ Application/src/gui/Gui.java
@@ -14,7 +14,10 @@ public class Gui extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Interfaces/Greeting/Input.fxml"));
+        String normal = "Interfaces/Greeting/Input.fxml";
+        String debug = "Interfaces/MainMenu/Menu.fxml";
+
+        Parent root = FXMLLoader.load(getClass().getResource(normal));
         primaryStage.setTitle("3D CNC Foam Cutter");
         primaryStage.setScene(new Scene(root,800, 600));
         primaryStage.show();
