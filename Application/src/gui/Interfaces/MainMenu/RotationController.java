9e7be73a2e94d5a88d1affa045138b13ef058708
Nicolas Lopez <nicolaslopezbravo@gmail.com>
2018-01-27 02:50:21 -0500

    added responsive label for file selection

--- Application/src/gui/Interfaces/MainMenu/RotationController.java
+++ Application/src/gui/Interfaces/MainMenu/RotationController.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -29,6 +30,7 @@ public class RotationController
      * The FXML elements of the tab.
      */
     public VBox vBox;
+    public Label fileName;
     
     //Static Fields
     
@@ -86,10 +88,12 @@ public class RotationController
         HBox hbox = new HBox();
         hbox.setSpacing(20);
         hbox.setStyle("-fx-padding: 40px;");
+        int size = 1;
         for (int i = 0; i < 10; i++)
         {
             Image image = new Image("file:src/gui/images/logo.PNG");
             ImageView pic = new ImageView(image);
+            size = (int)pic.getFitWidth();
             pic.setPreserveRatio(true);
             hbox.getChildren().add(pic);
         }
@@ -97,6 +101,16 @@ public class RotationController
         sp.setPrefSize(600, 270);
         sp.setContent(hbox);
         sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.setHmax(9.0); // set to the size of the arraylist aka gcodeTraces to keep track of current one
+
+        sp.hvalueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                fileName.setText(String.valueOf((new_val.intValue())));
+            }
+        });
+
+
         vBox.getChildren().add(0,sp);
 
         /* TODO once there are images to render
