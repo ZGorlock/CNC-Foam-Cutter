824feeab3ec1dffd2b0d32db85a8de4b98a52771
Nicolas Lopez <nicolaslopezbravo@gmail.com>
2018-03-29 17:08:27 -0400

    application gets feedback from grbl

--- Application/src/grbl/APIgrbl.java
+++ Application/src/grbl/APIgrbl.java
@@ -457,14 +457,12 @@ public class APIgrbl extends Thread
     {
         //  Parse line into coordinates
         String[] decomposed = line.split(",");
-
-        if (decomposed.length == 4) {
-            setStatus(decomposed[0].substring(1));
-            String[] first = decomposed[1].split(":");
-            setX(Double.parseDouble(first[1]));
-            setY(Double.parseDouble(decomposed[2]));
-            setZ(Double.parseDouble(decomposed[3]));
-        }
+        
+        setStatus(decomposed[0].substring(1));
+        String[] first = decomposed[1].split(":");
+        setX(Double.parseDouble(first[1]));
+        setY(Double.parseDouble(decomposed[2]));
+        setZ(Double.parseDouble(decomposed[3]));
 
         String x = String.format("%.2f", getCoordinateX());
         String y = String.format("%.2f", getCoordinateY());
